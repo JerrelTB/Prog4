c33b3e9b532e3df51a3174471c33d8874a47c311
JerrelTB <balkerjerrel@gmail.com>
2023-04-25 15:44:23 +0200

    res change

--- src/js/game.js
+++ src/js/game.js
@@ -5,7 +5,7 @@ import { Resources, ResourceLoader } from './resources.js'
 export class Game extends Engine {
 
     constructor() {
-        super({ width: 800, height: 600 })
+        super({ width: 1920, height: 1080 })
         this.start(ResourceLoader).then(() => this.startGame())
     }
 
